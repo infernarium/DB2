fe26b3e557681ba03a8484365946b9ae40bf216b
infernarium <mark.tumanyan@yandex.ru>
2023-11-05 00:16:39 +0300

    добавил подгрузку настроек добавил поле ввода цифр

--- Redis/1.py
+++ Redis/1.py
@@ -4,19 +4,35 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 
 
+class NumericEntry(tk.Entry):
+    def __init__(self, master=None, **kwargs):
+        # Регистрируем функцию валидации
+        self.validate_input = master.register(self.validate)
+        super().__init__(master, validate="key", validatecommand=(self.validate_input, '%P'), **kwargs)
+
+    def validate(self, new_text):
+        if (new_text == "" or new_text.isdigit()):
+            return True
+        return False
+
 class MainWindow(tk.Tk):
     def __init__(self, connection: redis.StrictRedis, parent = None):
         self.parent = parent
         super().__init__() 
-
+        
+        self.connection = connection
+        
         self.title("Потом чё-нибудь придумай")
-        self.geometry("800x600")
+        self.geometry("200x400")
         
         self.user_combobox = ttk.Combobox(self)
         
         self.settings_frame = tk.Frame(self, background="gray")
         self.settings_font_combobox = ttk.Combobox(self.settings_frame)
-        self.settings_size_entry = tk.Entry(self.settings_frame)
+        
+        self.dfhgadfhafdhg = (self.ValidateSize, "%P")
+        
+        self.settings_size_entry = NumericEntry(self.settings_frame)
         self.settings_color_combobox = ttk.Combobox(self.settings_frame)
         self.settings_font_style_combobox = ttk.Combobox(self.settings_frame)
         
@@ -26,6 +42,8 @@ class MainWindow(tk.Tk):
 
         self.Pack()
         self.Config()
+        self.LoadUsers()
+        self.FillFont()
     
     def Pack(self):
         self.user_combobox.pack(side="top",pady=[10,20])
@@ -43,8 +61,40 @@ class MainWindow(tk.Tk):
     def Config(self):
         self.save_button.config(command=self.Save)
         
+        self.user_combobox.config(state="readonly", width=40)
+        self.user_combobox.bind("<<ComboboxSelected>>", self.LoadSettingsData)
+        
+        self.settings_size_entry.bind()
+        
+        self.settings_font_combobox.config(state="readonly")
+        self.settings_color_combobox.config(state="readonly")
+        self.settings_font_style_combobox.config(state="readonly")
+        
     def Save(self):
         pass
+    
+    def FillFont(self):
+        self.settings_font_combobox["values"] = ["Arial", "Verdana"]
+        self.settings_color_combobox["values"] = ["Red", "Blue", "Yellow", "Green", "Black"]
+        self.settings_font_style_combobox["values"] = ["Thin", "Regular", "Bold"]
+    
+    def LoadUsers(self):
+        self.user_keys = self.connection.keys("user:*")
+        self.user_data = [[key.decode('utf-8'), json.loads(self.connection.get(key))] for key in self.user_keys]
+        self.user_names = [item[1].get("full_name") for item in self.user_data]
+        self.user_combobox["values"] = self.user_names
+        
+    def LoadSettingsData(self, event):
+        self.selected_name = self.user_combobox.get()
+        self.settings_data = [item[1].get("settings") for item in self.user_data if item[1].get("full_name") == self.selected_name][0]
+        
+        self.settings_font_combobox.set(self.settings_data.get("font"))
+        self.settings_size_entry.delete(0, "end")
+        self.settings_size_entry.insert(0, self.settings_data.get("size"))
+        self.settings_color_combobox.set(self.settings_data.get("color"))
+        self.settings_font_style_combobox.set(self.settings_data.get("font_style"))
+        
+        
 
 
 def fill_bd(connection: redis.StrictRedis):
@@ -54,7 +104,7 @@ def fill_bd(connection: redis.StrictRedis):
         "font": "Arial",
         "size": 10,
         "color": "red",
-        "font_style": "bold"
+        "font_style": "Thin"
         }
     }
     data_json = json.dumps(data)
@@ -67,7 +117,7 @@ def fill_bd(connection: redis.StrictRedis):
         "font": "Verdana",
         "size": 15,
         "color": "yellow",
-        "font_style": ""
+        "font_style": "Regular"
         }
     }
     data_json = json.dumps(data)
@@ -80,18 +130,12 @@ def fill_bd(connection: redis.StrictRedis):
         "font": "Arial",
         "size": 6,
         "color": "blue",
-        "font_style": "italic"
+        "font_style": "Bold"
         }
     }
     data_json = json.dumps(data)
     key = "user:3"
     connection.set(key, data_json)
-    
-    # Десериализуйте данные из JSON в словарь
-    stored_data = json.loads(connection.get("user:3"))
-
-    # Выведите сохраненные данные
-    print(stored_data)
 
 
 if __name__ == "__main__":
